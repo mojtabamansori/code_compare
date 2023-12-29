964f345d3c5656cfb4e665d967090d8c882d0c2f
mojtabamansori <13mrlien13@gmail.com>
2023-12-29 20:03:08 +0330

    proj done

--- code_compare.py
+++ code_compare.py
@@ -15,4 +15,45 @@ df = pd.DataFrame.from_records(results, columns=columns)
 cursor.close()
 conn.close()
 df = np.array(df)
-print(df[:,2])
+file_path = r'E:\code parking\database\rosobi.xlsx'
+df_csv = np.array(pd.read_excel(file_path))
+
+pelak_acc = df[:, 2]
+pelak_csv = df_csv[:, 6]
+pelak_brabar = 0
+pelak_seen = set()
+
+for i in pelak_acc:
+    for i2 in pelak_csv:
+        if i2 == i and i not in pelak_seen:
+            pelak_brabar = pelak_brabar + 1
+            pelak_seen.add(i)
+
+print('pelak brabar = ', pelak_brabar)
+
+motor_number_acc = df[:, 5]
+motor_number_csv = df_csv[:, 3]
+motor_brabar = 0
+motor_seen = set()
+
+for i in motor_number_acc:
+    for i2 in motor_number_csv:
+        if i2 == i and i not in motor_seen:
+            motor_brabar = motor_brabar + 1
+            motor_seen.add(i)
+
+print('motor_brabar = ', motor_brabar)
+
+tane_number_acc = df[:, 4]
+tane_number_csv = df_csv[:, 2]
+tane_brabar = 0
+tane_seen = set()
+
+for i in tane_number_acc:
+    for i2 in tane_number_csv:
+        if i2 == i and i not in tane_seen:
+            tane_brabar = tane_brabar + 1
+            tane_seen.add(i)
+
+print('tane_brabar = ', tane_brabar)
+
